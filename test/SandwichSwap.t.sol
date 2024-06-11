ba85b80e88c798cd1b688d94f7fc79c3b47a46f7
tanim0la <izukfred@gmail.com>
2024-06-11 07:31:28 +0100

    puzzles

--- test/SandwichSwap.t.sol
+++ test/SandwichSwap.t.sol
@@ -10,7 +10,6 @@ contract SandwichSwapTest is Test {
     Victim public victim;
     address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    // address public pool = 0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
     address public router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
     function setUp() public {
